3faedffc08f137e22705e539dd618e40a6f38f47
sneharatnani <sneharatnani5@gmail.com>
2022-06-09 16:38:27 +0530

    style links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between px-8 h-[10vh] items-center text-lightGrey">
-      <Link to="/" className="text-4xl font-raleway">
-        S<span className="text-green">R.</span>
-      </Link>
-      <div className="flex gap-6 capitalize">
-        <Link to="/" className="text-green font-medium">
-          home
+    <header className="shadow-3xl font-[Poppins]">
+      <nav className="w-11/12 m-auto flex justify-between h-[10vh] items-center text-lightGrey">
+        <Link to="/" className="text-4xl font-raleway">
+          S<span className="text-green">R.</span>
         </Link>
-        <Link to="/">about</Link>
-        <Link to="/">projects</Link>
-      </div>
-    </nav>
+        <div className="flex gap-6 capitalize text-lg">
+          <Link
+            to="/"
+            className="border-b-2 border-b-transparent hover:border-b-green transition duration-300 ease-out"
+          >
+            home
+          </Link>
+          <Link to="/">about</Link>
+          <Link to="/">projects</Link>
+        </div>
+      </nav>
+    </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
-    colors: {
-      green: "#72b626",
-      lightGrey: "#666",
-    },
     extend: {
+      colors: {
+        green: "#72b626",
+        lightGrey: "#666",
+      },
       fontFamily: {
         raleway: "'Raleway', 'sans - serif'",
+      },
+      boxShadow: {
+        "3xl": "0 -3px 6px 0",
       },
     },
   },
