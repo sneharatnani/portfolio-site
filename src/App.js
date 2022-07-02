47541de6fbb01f409887056ee20d68234bbf257f
sneharatnani <sneharatnani5@gmail.com>
2022-07-02 16:54:30 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import { Route, Routes } from "react-router-dom";
 import Projects from "./pages/Projects.js";
 import Home from "./pages/Home.js";
 import About from "./pages/About.js";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   const { theme } = useContext(ThemeContext);
   return (
     <div className={`${theme} font-poppins font-normal tracking-wide`}>
       <div className="dark:bg-gray-900 text-gray-700 dark:text-white">
-        <Navbar />
-        <Routes>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+
+        {/* <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
-        </Routes>
+        </Routes> */}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 import ThemeContextProvider from "./themeContext.js";
 import "./assets/styles.css";
 
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <Router>
-        <App />
-      </Router>
+      {/* <Router> */}
+      <App />
+      {/* </Router> */}
     </ThemeContextProvider>
   </React.StrictMode>
 );
