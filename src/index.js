1a7005a10159384e8dda63c70c0b2885d62ef0ae
sneharatnani <sneharatnani5@gmail.com>
2022-07-02 14:48:36 +0530

    change to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import ThemeContextProvider from "./themeContext.js";
 import "./assets/styles.css";
 
