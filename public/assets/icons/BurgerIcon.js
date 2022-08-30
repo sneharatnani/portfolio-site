c314ca790ebf16d87e9f29aac5b653e488733358
sneharatnani <sneharatnani5@gmail.com>
2022-08-30 13:19:31 +0530

    .

---
+++ public/assets/icons/BurgerIcon.js
@@ -0,0 +1,13 @@
+export default function BurgerIcon() {
+  return (
+    <svg
+      className="stroke-black h-6 w-6 dark:stroke-white"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path>
+    </svg>
+  );
+}
